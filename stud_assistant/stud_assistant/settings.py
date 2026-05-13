194c31a75a33ac97dc056d08672d8f62bbee9d48
retives <srgtkrvvv@proton.me>
2026-05-13 11:22:14 +0300

    Protected the secret key

--- stud_assistant/stud_assistant/settings.py
+++ stud_assistant/stud_assistant/settings.py
@@ -9,26 +9,13 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dp^9tx9(=iu-wybcs=!*rb1gni(z)6y4q7t56-=8o)+9*8@9i%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
