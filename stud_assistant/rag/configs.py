275a99520a2bfe5664ad1892ad3fd2906b72ceb5
retives <srgtkrvvv@proton.me>
2026-05-18 22:10:49 +0300

    Added multi vector configs + ollama host variable

--- stud_assistant/rag/configs.py
+++ stud_assistant/rag/configs.py
@@ -3,8 +3,16 @@ from langchain_community.document_loaders.word_document import Docx2txtLoader
 from playwright.sync_api import sync_playwright
 # Vector storage
 VECTORSTORE_PATH = "docs/vectorstore"
-CHUNK_SIZE=3000
-CHUNK_OVERLAP_SIZE=200
+FILESTORE_PATH = "docs/docstore"
+PARENT_CHUNK_SIZE=3000
+PARENT_CHUNK_OVERLAP_SIZE=200
+
+CHILD_CHUNK_SIZE=700
+CHILD_CHUNK_OVERLAP_SIZE=100
+
+DOC_ID = "parent_id"
+
+OLLAMA_HOST = "http://ollama:11434"
 
 FILE_PATHS = [
     {
