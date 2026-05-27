3c91274156c3dce64a0cfe80e6545850f1905d8c
retives <srgtkrvvv@proton.me>
2026-05-28 01:07:19 +0300

    Redirect straight to the chat page

--- stud_assistant/urls.py
+++ stud_assistant/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', RedirectView.as_view(url='/chat', permanent=True)),
     path("", include("accounts.urls")),
     path("chat/", include("rag.urls"))
 ]
